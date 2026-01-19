efbdc6a40bde254dbd67f981a7de80d959712cab
Giovanni La Scala <116294748+gyowyx96@users.noreply.github.com>
2026-01-19 09:57:31 +0100

    change connection with pool

--- db.js
+++ db.js
@@ -1,16 +1,25 @@
 const mysql = require("mysql2");
-const connection = mysql.createConnection({
+
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: "ltnya0pnki2ck9w8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
   user: "o22oiac8jp4a0dw9",
   password: "ko4lh4ad1wlncdwm",
   database: "gigwx6tnzj7haptc",
+  waitForConnections: true,
+  queueLimit: 0,
 });
-connection.connect((err) => {
+
+// Test opzionale (una sola volta)
+pool.getConnection((err, conn) => {
   if (err) {
     console.error("❌ Errore MySQL:", err);
   } else {
-    console.log("✅ Connesso a MySQL con successo");
+    console.log("✅ Pool MySQL pronto");
+    conn.release();
   }
 });
 
-module.exports = connection;
+module.exports = pool;
+
